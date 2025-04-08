465cabcbdfe1c22c8ab728f7bc39de5fc3c73311
Your Namesomumanik <manikchowdhuri@gmail.com>
2025-04-08 13:43:30 +0530

    changes34

--- src/Common/Testing.jsx
+++ src/Common/Testing.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
-import { FaUserAlt } from 'react-icons/fa'
-import { FaBagShopping, FaBarsStaggered, FaClockRotateLeft, FaMessage } from 'react-icons/fa6'
+import { FaSlidersH, FaUserAlt } from 'react-icons/fa'
+import { FaBagShopping, FaBarsStaggered, FaClockRotateLeft, FaMessage, FaUserPen } from 'react-icons/fa6'
+import { FcFaq } from 'react-icons/fc'
 import { IoMdRadioButtonOn } from 'react-icons/io'
 import { IoChevronDown } from 'react-icons/io5'
+import { LuNotepadText } from 'react-icons/lu'
 import { MdInvertColors } from 'react-icons/md'
 import { SiMaterialdesign } from 'react-icons/si'
+import { TbGpsFilled } from 'react-icons/tb'
 
 export default function Testing() {
     let [passwordstatus, setPasswordstatus] = useState(false)
@@ -13,15 +16,15 @@ export default function Testing() {
     let [passwordstatusMat, setPasswordstatusMat] = useState(false)
     let [passwordstatusParCa, setPasswordstatusParCa] = useState(false)
     let [passwordstatusSubCa, setPasswordstatusSubCa] = useState(false)
-    let [passwordstatusSubSubCa, setpasswordstatusSubSubCa] = useState(true)
-    let [passwordstatusProd, setpasswordstatusProd] = useState(true)
-    let [passwordstatusWhyChooUs, setpasswordstatusWhyChooUs] = useState(true)
-    let [passwordstatusOrde, setpasswordstatusOrde] = useState(true)
-    let [passwordstatusSlider, setpasswordstatusSlider] = useState(true)
-    let [passwordstatusCountr, setpasswordstatusCountr] = useState(true)
-    let [passwordstatusTesti, setpasswordstatusTesti] = useState(true)
-    let [passwordstatusFaq, setpasswordstatusFaq] = useState(true)
-    let [passwordstatusTerms, setpasswordstatusTermsa] = useState(true)
+    let [passwordstatusSubSubCa, setpasswordstatusSubSubCa] = useState(false)
+    let [passwordstatusProd, setpasswordstatusProd] = useState(false)
+    let [passwordstatusWhyChooUs, setpasswordstatusWhyChooUs] = useState(false)
+    let [passwordstatusOrde, setpasswordstatusOrde] = useState(false)
+    let [passwordstatusSlider, setpasswordstatusSlider] = useState(false)
+    let [passwordstatusCountr, setpasswordstatusCountr] = useState(false)
+    let [passwordstatusTesti, setpasswordstatusTesti] = useState(false)
+    let [passwordstatusFaq, setpasswordstatusFaq] = useState(false)
+    let [passwordstatusTerms, setpasswordstatusTermsa] = useState(false)
 
     return (
         <div>
@@ -228,7 +231,7 @@ export default function Testing() {
                 {/* Why Choose US Link Start */}
                 <li onClick={() => setpasswordstatusWhyChooUs(!passwordstatusWhyChooUs)} className="cursor-pointer">
                     <div className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                        <FaClockRotateLeft/>
+                        <FaClockRotateLeft />
                         <span className="flex-1 ms-3 whitespace-nowrap">Why Choose US</span>
                         <IoChevronDown />
                     </div>
@@ -251,6 +254,142 @@ export default function Testing() {
                     </ul>
                 </li>
                 {/* Why Choose US Link Link End */}
+
+                {/* Orders Link Start */}
+                <li onClick={() => setpasswordstatusOrde(!passwordstatusOrde)} className="cursor-pointer">
+                    <div className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <LuNotepadText />
+                        <span className="flex-1 ms-3 whitespace-nowrap">Orders</span>
+                        <IoChevronDown />
+                    </div>
+                    <ul className={passwordstatusOrde ? "block" : "hidden"}><a href="/category/sub-sub-category/add">
+                        <li>
+                            <button className="flex items-center p-2 w-full text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                <IoMdRadioButtonOn />
+                                <span className="font-semibold ms-4 text-[14px] whitespace-nowrap">Add Orders</span>
+                            </button>
+                        </li></a>
+                        <a href="/category/sub-sub-category/view">
+                            <li>
+                                <button className="flex items-center p-2 w-full text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                    <IoMdRadioButtonOn />
+
+                                    <span className="font-semibold ms-4 text-[14px] whitespace-nowrap">View Orders</span>
+                                </button>
+                            </li>
+                        </a>
+                    </ul>
+                </li>
+                {/* Orders Link Link End */}
+
+                {/* Sliders Link Start */}
+                <li onClick={() => setpasswordstatusSlider(!passwordstatusSlider)} className="cursor-pointer">
+                    <div className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <FaSlidersH />
+                        <span className="flex-1 ms-3 whitespace-nowrap">Sliders</span>
+                        <IoChevronDown />
+                    </div>
+                    <ul className={passwordstatusSlider ? "block" : "hidden"}><a href="/category/sub-sub-category/add">
+                        <li>
+                            <button className="flex items-center p-2 w-full text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                <IoMdRadioButtonOn />
+                                <span className="font-semibold ms-4 text-[14px] whitespace-nowrap">Add Sliders</span>
+                            </button>
+                        </li></a>
+                        <a href="/category/sub-sub-category/view">
+                            <li>
+                                <button className="flex items-center p-2 w-full text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                    <IoMdRadioButtonOn />
+
+                                    <span className="font-semibold ms-4 text-[14px] whitespace-nowrap">View Sliders</span>
+                                </button>
+                            </li>
+                        </a>
+                    </ul>
+                </li>
+                {/* Sliders Link Link End */}
+
+                {/* Country Link Start */}
+                <li onClick={() => setpasswordstatusCountr(!passwordstatusCountr)} className="cursor-pointer">
+                    <div className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <TbGpsFilled />
+                        <span className="flex-1 ms-3 whitespace-nowrap">Country</span>
+                        <IoChevronDown />
+                    </div>
+                    <ul className={passwordstatusCountr ? "block" : "hidden"}><a href="/category/sub-sub-category/add">
+                        <li>
+                            <button className="flex items-center p-2 w-full text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                <IoMdRadioButtonOn />
+                                <span className="font-semibold ms-4 text-[14px] whitespace-nowrap">Add Country</span>
+                            </button>
+                        </li></a>
+                        <a href="/category/sub-sub-category/view">
+                            <li>
+                                <button className="flex items-center p-2 w-full text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                    <IoMdRadioButtonOn />
+
+                                    <span className="font-semibold ms-4 text-[14px] whitespace-nowrap">View Country</span>
+                                </button>
+                            </li>
+                        </a>
+                    </ul>
+                </li>
+                {/* Country Link End */}
+
+                {/* Testimonials Link Start */}
+                   <li onClick={() => setpasswordstatusTesti(!passwordstatusTesti)} className="cursor-pointer">
+                    <div className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <FaUserPen />
+                        <span className="flex-1 ms-3 whitespace-nowrap">Testimonials</span>
+                        <IoChevronDown />
+                    </div>
+                    <ul className={passwordstatusTesti ? "block" : "hidden"}><a href="/category/sub-sub-category/add">
+                        <li>
+                            <button className="flex items-center p-2 w-full text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                <IoMdRadioButtonOn />
+                                <span className="font-semibold ms-4 text-[14px] whitespace-nowrap">Add Testimonials</span>
+                            </button>
+                        </li></a>
+                        <a href="/category/sub-sub-category/view">
+                            <li>
+                                <button className="flex items-center p-2 w-full text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                    <IoMdRadioButtonOn />
+
+                                    <span className="font-semibold ms-4 text-[14px] whitespace-nowrap">View Testimonials</span>
+                                </button>
+                            </li>
+                        </a>
+                    </ul>
+                </li>
+                {/* Testimonials Link Link End */}
+
+                 {/* Faqs Link Start */}
+                 <li onClick={() => setpasswordstatusFaq(!passwordstatusFaq)} className="cursor-pointer">
+                    <div className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <FcFaq />
+                        <span className="flex-1 ms-3 whitespace-nowrap">Faqs</span>
+                        <IoChevronDown />
+                    </div>
+                    <ul className={passwordstatusFaq ? "block" : "hidden"}><a href="/category/sub-sub-category/add">
+                        <li>
+                            <button className="flex items-center p-2 w-full text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                <IoMdRadioButtonOn />
+                                <span className="font-semibold ms-4 text-[14px] whitespace-nowrap">Add Faqs</span>
+                            </button>
+                        </li></a>
+                        <a href="/category/sub-sub-category/view">
+                            <li>
+                                <button className="flex items-center p-2 w-full text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                    <IoMdRadioButtonOn />
+
+                                    <span className="font-semibold ms-4 text-[14px] whitespace-nowrap">View Faqs</span>
+                                </button>
+                            </li>
+                        </a>
+                    </ul>
+                </li>
+                {/* Faqs Link Link End */}
+
             </ul>
 
         </div>
