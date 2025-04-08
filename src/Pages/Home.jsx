450f5f10a6504a7215e14a00abae479aef667b29
Your Namesomumanik <manikchowdhuri@gmail.com>
2025-04-08 16:02:05 +0530

    changes7979

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -23,7 +23,7 @@ export default function Home() {
             <div className='fixed top-[15%] left-[20%] p-5 lg:w-[80%]  mx-auto'>
                 <div className=' grid grid-cols-3 my-5 gap-4'>
                     
-                    <div className='bg-indigo-600 rounded-[10px] h-[200px]'>
+                    <div className='bg-indigo-500 rounded-[10px] h-[200px]'>
                         <div className='flex justify-between pt-4 px-4 items-center'>
                             <h1 className='text-[25px] font-bold text-white'>26K <span className='text-[20px]'>(-12.4% ↓)</span></h1>
                             <CiMenuKebab className='text-[20px] font-bold text-white' />
@@ -31,7 +31,7 @@ export default function Home() {
                         <p className='text-[25px] font-semibold text-white px-4'>Users</p>
                     </div>
 
-                    <div className='bg-blue-500 rounded-[10px] h-[200px]'>
+                    <div className='bg-blue-400 rounded-[10px] h-[200px]'>
                         <div className='flex justify-between pt-4 px-4 items-center'>
                             <h1 className='text-[25px] font-bold text-white'>$6,200  <span className='text-[20px]'>(40.9% ↑)</span></h1>
                             <CiMenuKebab className='text-[20px] font-bold text-white' />
@@ -39,7 +39,7 @@ export default function Home() {
                         <p className='text-[25px] font-semibold text-white px-4'>Product</p>
                     </div>
 
-                    <div className='bg-yellow-400 rounded-[10px] h-[200px]'>
+                    <div className='bg-yellow-300 rounded-[10px] h-[200px]'>
                         <div className='flex justify-between pt-4 px-4 items-center'>
                             <h1 className='text-[25px] font-bold text-white'>2.49%<span className='text-[20px]'>(84.7% ↑)</span></h1>
                             <CiMenuKebab className='text-[20px] font-bold text-white' />
@@ -47,7 +47,7 @@ export default function Home() {
                         <p className='text-[25px] font-semibold text-white px-4'>Category</p>
                     </div>
 
-                    <div className='bg-red-700 rounded-[10px] h-[200px]'>
+                    <div className='bg-red-500 rounded-[10px] h-[200px]'>
                         <div className='flex justify-between pt-4 px-4 items-center'>
                             <h1 className='text-[25px] font-bold text-white'>44K  <span className='text-[20px]'>(-23.6% ↓)</span></h1>
                             <CiMenuKebab className='text-[20px] font-bold text-white' />
